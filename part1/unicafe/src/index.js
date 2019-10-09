cd4cf0e68ee8dc22f6af4c8660b8b6d96a5064f2
Sergiu <ctinsergiu@gmail.com>
2019-10-09 21:20:47 +0300

    1.11*: unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({text}) => {
+  return <h1>{text}</h1>;
+}
 
 const Button = ({onClick, text}) => (
   <button onClick={onClick}>
@@ -8,14 +11,34 @@ const Button = ({onClick, text}) => (
   </button>
 )
 
-const Display = ({feedback, fb, text}) => (
-  <p> {text} {feedback} {fb}</p>
-)
+const Statistic = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
+const Statistics = ({good, neutral, bad }) => {
+  const all = good+neutral+bad;
+  const average = (good-bad)/all;
+  const positive = good/all*100;
+
+  if (all>0) {
+    return (
+      <table><tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={all} />
+            <Statistic text="average" value={average} />
+            <Statistic text="positive" value={positive+'%'} />
+      </tbody></table>
+    )
+  } else {
+    return <div>No feedback given</div>
+}
+  }
+  
 const App = () => {
   const [feedback, setFeedback] = useState({
     good: 0, neutral: 0, bad: 0
   })
+ 
 
   const handleGood = () => {
     setFeedback({ ...feedback, good: feedback.good + 1 });
@@ -29,15 +52,13 @@ const App = () => {
 
   return (
     <div>
-        <h1>give feedback</h1>
+         <Header text="Give feedback"/>
         <Button onClick={handleGood} text="good"/>
         <Button onClick={handleNeutral} text="neutral"/>
         <Button onClick={handleBad} text="bad"/>
 
-        <h1>statistics</h1>
-        <Display text="good" feedback={feedback.good} />
-        <Display text="neutral" feedback={feedback.neutral} />
-        <Display text="bad" feedback={feedback.bad} />
+        <Header text="statistics"/>
+        <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad}/>
     </div>
   )
 }
