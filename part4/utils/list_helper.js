49be4683d1a2ab42c9dcdbf10bb57f4ab6a6b0f0
Sergiu <ctinsergiu@gmail.com>
2019-10-21 13:19:40 +0300

    4.4: helper functions and unit tests, step2 completed

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
   return 1;
 }
+const totalLikes = (blogs) =>{
+
+  if (blogs.length === 0 ) return 0
+  
+  const result = blogs.reduce((a,b) => {
+    return { likes: a.likes + b.likes }
+  })
+    
+  return result.likes
+
+}
+
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
