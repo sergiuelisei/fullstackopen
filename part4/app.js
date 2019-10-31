3aa2914009adf0290728f0edc67f9bddd66db9a8
Sergiu <ctinsergiu@gmail.com>
2019-10-31 15:07:32 +0200

    4.15: bloglist expansion, step4 completed

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
@@ -25,6 +27,7 @@ app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
