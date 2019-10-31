bcaec7bd23442ee37639998c63f3a860d215d7a6
Sergiu <ctinsergiu@gmail.com>
2019-10-31 16:53:22 +0200

    4.20*: bloglist expansion, step9 completed

--- part4/app.js
+++ part4/app.js
@@ -24,6 +24,7 @@ mongoose
 
 app.use(cors());
 
+app.use(middleware.tokenExtractor);
 app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(middleware.requestLogger);
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = (request) => {
-	const authorization = request.get('authorization');
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.substring(7);
-	}
-	return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 	response.json(blogs.map((blog) => blog.toJSON()));
@@ -32,8 +24,8 @@ blogsRouter.get('/:id', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
 	const body = request.body;
 
-	const token = getTokenFrom(request);
-
+	// const token = getTokenFrom(request);
+	const token = request.token;
 	try {
 		const decodedToken = jwt.verify(token, process.env.SECRET);
 		if (!token || !decodedToken.id) {
