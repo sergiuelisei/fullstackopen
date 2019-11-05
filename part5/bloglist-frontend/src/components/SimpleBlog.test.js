aeece9061ddd3f20a978bb4009f1a02381e995b3
Sergiu <ctinsergiu@gmail.com>
2019-11-05 13:20:25 +0200

    5.14: Blog list tests, step2 completed

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', () => {
@@ -18,4 +18,24 @@ test('renders content', () => {
 	expect(component.container).toHaveTextContent('Sergiu')
 	expect(component.container).toHaveTextContent(1)
 
+})
+
+test('clicking the like button twice calls event handler twice', () => {
+	const blog = {
+		title: 'testing a component',
+		author: 'Sergiu',
+		likes: 1
+	}
+
+	const mockHandler = jest.fn()
+
+	const { getByText } = render(
+		<SimpleBlog blog={blog} onClick={mockHandler} />
+	)
+
+	const button = getByText('like')
+	fireEvent.click(button)
+	fireEvent.click(button)
+
+	expect(mockHandler.mock.calls.length).toBe(2)
 })
